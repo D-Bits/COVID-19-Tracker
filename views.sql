96ab1707dc6611327ae4ecef749490c90bea8757
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-03-27 14:16:50 -0700

    Updated deaths_by_country view

--- views.sql
+++ views.sql
@@ -1,7 +1,12 @@
 
--- Show deaths by country in ascending order
+-- Show deaths by country in descending order
 CREATE VIEW deaths_by_country 
 AS 
-SELECT "Country", "TotalDeaths"
+SELECT "Country", "TotalDeaths", "NewDeaths" 
 FROM public.summary
 ORDER BY TotalDeaths ASC;
+
+-- Show the number of new cases in descending order
+CREATE VIEW new_cases_by_country
+AS 
+SELECT 
