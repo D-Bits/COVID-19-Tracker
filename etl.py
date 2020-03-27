64f1e41a9be634b938b4ae9705fc1090e7927172
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-03-27 13:24:07 -0700

    More ETL work.

--- etl.py
+++ etl.py
@@ -1,5 +1,6 @@
-from config import summary_json, engine
+from config import summary_json, engine, db_connection
 from pandas.errors import EmptyDataError
+from psycopg2.errors import UndefinedTable
 import pandas as pd
 
 
@@ -12,14 +13,17 @@ def summary_etl():
         # Drop slug field from DataFrame
         cleaned_data = df.drop(['Slug'], axis=1)
         # Dump cleaned API data to temp CSV file
-        cleaned_data.to_csv('data/summary.csv')
-        # Create a cursor
-
-
-
-        #cleaned_data.to_sql('summary', engine, index_label='id', if_exists='append', method='multi')
+        cleaned_data.to_csv("data/summary.csv", sep=",")
+        # Write DataFrame to summary table in db
+        cleaned_data.to_sql('summary', engine, index_label='id', if_exists='append', method='multi')
 
     # Throw exception if DataFrame is empty
     except EmptyDataError:
         input("Error: No data in DataFrame!")
+    # Throw exception if table does not exist in DB.
+    except UndefinedTable:
+        input("Error: Table does not exist in database! Press enter to exit.")
+    # Throw exception is data source cannot be found
+    except FileNotFoundError:
+        input('Error: Data source cannot be found! Press enter to exit.')
 
