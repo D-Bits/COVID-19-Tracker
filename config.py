e3cce9cc131b3b275b3c0ff88a432380a319e9bf
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-03-27 11:39:52 -0700

    Added config file

---
+++ config.py
@@ -0,0 +1,26 @@
+from dotenv import load_dotenv 
+from os import getenv
+from psycopg2 import connect
+from sqlalchemy import create_engine
+
+
+# Enable load environment variables from .env file
+load_dotenv()
+
+# Load db credentials from environment vars
+db_host = getenv('DB_HOST')
+db_name = getenv("DB_NAME")
+db_user = getenv('DB_USER')
+db_pass = getenv('DB_PASS')
+db_port = getenv('DB_PORT')
+
+# Establish db connection
+db_connection = connect(database=db_name, host=db_host, user=db_user, password=db_pass)
+
+# Create a SQLAlchemy engine to execute queries
+engine = create_engine(f"postgresql+psycopg2://{db_user}:{db_pass}@{db_host}:{db_port}/covid19")
+
+"""
+Define API endpoints
+"""
+
