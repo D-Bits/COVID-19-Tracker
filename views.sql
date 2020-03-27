66a62253b415205586a76aa85c49d8a42af90629
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-03-27 14:24:23 -0700

    Added new views.

--- views.sql
+++ views.sql
@@ -4,9 +4,18 @@ CREATE VIEW deaths_by_country
 AS 
 SELECT "Country", "TotalDeaths", "NewDeaths" 
 FROM public.summary
-ORDER BY TotalDeaths ASC;
+ORDER BY "TotalDeaths" DESC, "NewDeaths" DESC;
 
 -- Show the number of new cases in descending order
 CREATE VIEW new_cases_by_country
 AS 
-SELECT 
+SELECT "Country", "NewConfirmed"
+FROM public.summary
+ORDER BY "NewConfirmed" DESC;
+
+-- Show the number of recoveries by country
+CREATE VIEW recoveries_by_country
+AS 
+SELECT "Country", "TotalRecovered", "NewRecovered"
+FROM public.summary
+ORDER BY "TotalRecovered" DESC;
