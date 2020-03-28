5778bc16750efa16bcf66fe5886d77702133d6da
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-03-28 12:53:39 -0700

    Consolidated redundant functions.

--- etl.py
+++ etl.py
@@ -1,19 +1,28 @@
 from config import summary_json, usa_json, engine, db_connection
+from pandas.io.json import json_normalize
 from pandas.errors import EmptyDataError
 from psycopg2.errors import UndefinedTable
 import pandas as pd
 
 
-# Clean and load data from Summary API endpoint into db
-def summary_etl():
+"""
+Params:
+- data_src = the API endpoint
+- table = the db table the data is to be written to
+- dropped_records = a list of row numbers to be dropped prior to ingestion, 
+                    or 0 if no records are to be dropped.
+"""
+def data_etl(data_src, table, dropped_records):
 
     try:
         # Load API JSON data into DataFrame
-        df = pd.DataFrame(summary_json['Countries'])
+        df = pd.DataFrame(data_src)
         # Drop slug field from DataFrame
-        cleaned_data = df.drop(labels=[93, 98, 165, 171, 199, 219])
+        cleaned_data = df.drop(dropped_records)
         # Write DataFrame to summary table in db
-        cleaned_data.to_sql('summary', engine, index_label='id', if_exists='replace', method='multi')
+        cleaned_data.to_sql(table, engine, index_label='id', if_exists='replace', method='multi')
+        # Show the no. of records written to the db
+        print(f"{len(cleaned_data)} records successfully written to 'summary' table.")
 
     # Throw exception if DataFrame is empty
     except EmptyDataError:
@@ -21,12 +30,3 @@ def summary_etl():
     # Throw exception if table does not exist in DB.
     except UndefinedTable:
         input("Error: Table does not exist in database! Press enter to exit.")
-
-
-# TODO: Clean and load data for confirmed U.S. cases
-def usa_etl():
-
-    try:
-        df = pd.DataFrame()
-    except:
-        pass
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from etl import summary_etl
+from config import summary_json, usa_json
+from etl import summary_etl, data_etl
 
 
 if __name__ == "__main__":
     
-    summary_etl()
+    # Load data from the summary API into summary table
+    data_etl(summary_json['Countries'], 'summary', [93, 98, 165, 171, 199, 219])
+    # Load data for confirmed US cases into us_cases table
+    data_etl(usa_json, 'us_cases', 0)
