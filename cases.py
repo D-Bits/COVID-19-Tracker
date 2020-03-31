ceed49e2dea3cd073cc61a9c1bdeb84f0fe36314
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-03-30 20:54:19 -0700

    Added top 20 cases.

--- cases.py
+++ cases.py
@@ -10,17 +10,22 @@ def all_cases():
     df = pd.DataFrame(summary_json['Countries'])
     # Show only "NewConfirmed" and "TotalConfirmed", and countries names
     filtered_data = df.filter(items=['Country', 'NewConfirmed', 'TotalConfirmed'])
-
     # Drop redundant records obtained from API
     cleaned_data = filtered_data.drop([0, 93, 98, 165, 171, 199, 219, 221])
 
-    print(cleaned_data)
+    return cleaned_data
+
 
 # Show the top 20 countries with the most cases
 def top_20():
 
     df = pd.DataFrame(summary_json['Countries'])
-    
+    # Show only "NewConfirmed" and "TotalConfirmed", and countries names
+    filtered_data = df.filter(items=['Country', 'NewConfirmed', 'TotalConfirmed'])
+    # Drop redundant records obtained from API
+    cleaned_data = filtered_data.drop([0, 93, 98, 165, 171, 199, 219, 221])
+    # Filter top countries
+    return cleaned_data.nlargest(20, 'TotalConfirmed')
 
 
-all_cases()
+top_20()
