3f533f6a499d6c3213c08e7af39d1731ac16824d
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-03-30 20:43:22 -0700

    Added data analysis for cases.

---
+++ cases.py
@@ -0,0 +1,26 @@
+"""
+Analysis for statistics of cases per country.
+"""
+from config import summary_json
+import pandas as pd 
+
+
+def all_cases():
+
+    df = pd.DataFrame(summary_json['Countries'])
+    # Show only "NewConfirmed" and "TotalConfirmed", and countries names
+    filtered_data = df.filter(items=['Country', 'NewConfirmed', 'TotalConfirmed'])
+
+    # Drop redundant records obtained from API
+    cleaned_data = filtered_data.drop([0, 93, 98, 165, 171, 199, 219, 221])
+
+    print(cleaned_data)
+
+# Show the top 20 countries with the most cases
+def top_20():
+
+    df = pd.DataFrame(summary_json['Countries'])
+    
+
+
+all_cases()
