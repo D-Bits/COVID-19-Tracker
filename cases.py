0b8bed7224e19f2388b0977525847b03353df5ed
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-03-30 23:07:38 -0700

    Added cases by country.

--- cases.py
+++ cases.py
@@ -2,6 +2,7 @@
 Analysis for statistics of cases per country.
 """
 from config import summary_json
+from requests import get
 import pandas as pd 
 
 
@@ -28,4 +29,18 @@ def top_20():
     return cleaned_data.nlargest(20, 'TotalConfirmed')
 
 
-top_20()
+# Find all cases in a specific country
+def cases_per_country(country_name):
+
+    # Make GET request to API endpoint, and extract JSON data
+    endpoint = get(f'https://api.covid19api.com/country/{country_name}/status/confirmed/live')
+    json_data = endpoint.json()
+
+    df = pd.DataFrame(json_data)
+    # Sort cases by date, starting with most recent
+    ordered_values = df.sort_values('Date', ascending=True)
+
+    return df
+
+
+print(cases_per_country('us'))
