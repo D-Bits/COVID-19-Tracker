5cf2afbfd4d7f7426b56662f6c4643ae1289509f
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-03-30 20:46:21 -0700

    Added summary of all data.

---
+++ summary.py
@@ -0,0 +1,14 @@
+"""
+Summary of COVID-19 data per country.
+"""
+from config import summary_json
+import pandas as pd 
+
+
+def all_cases():
+
+    df = pd.DataFrame(summary_json['Countries'])
+    # Drop redundant records obtained from API
+    cleaned_data = df.drop([0, 93, 98, 165, 171, 199, 219])
+
+    print(cleaned_data)
