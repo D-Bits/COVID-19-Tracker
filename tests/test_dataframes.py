6d0eb215fe9a0d707ba042016d0f3a099311e85d
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-04-03 02:16:02 -0700

    Added new unit tests.

---
+++ tests/test_dataframes.py
@@ -0,0 +1,11 @@
+"""
+Unit tests for DataFrames
+"""
+from unittest import TestCase
+from config import usa_json, summary_json
+import pandas as pd 
+
+
+class DataFrameTests(TestCase):
+
+    pass
---
+++ tests/test_requests.py
@@ -0,0 +1,15 @@
+"""
+Unit tests for DataFrames
+"""
+from unittest import TestCase
+from config import usa_cases, summary
+
+
+# Test requests to API endpoints
+class RequestsTests(TestCase):
+
+    # Test response codes from API endpoints
+    def test_endpoints(self):
+
+        self.assertEqual(summary.status_code, 200)
+        self.assertEqual(usa_cases.status_code, 200)
