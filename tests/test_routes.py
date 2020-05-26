a1d64cd20629b528f81986e15448ff0cef465346
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-05-26 15:40:52 -0700

    PEP8 formatting.

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,17 +1,16 @@
 from unittest import TestCase
 from app import app
 
-
 """
 Unit testing for routes
 """
+
+
 class RoutesTests(TestCase):
 
     # Test index route(s) returns a 200 response
     def test_index_route(self):
-
         with app.test_client() as tc:
-
             response = tc.get('/')
             redirect_response = tc.get('/home')
             self.assertEqual(response.status_code, 200)
@@ -19,40 +18,30 @@ class RoutesTests(TestCase):
 
     # Test about page route
     def test_about_route(self):
-
         with app.test_client() as tc:
-
             response = tc.get('/about')
             self.assertEqual(response.status_code, 200)
 
     # Test cases route
     def test_cases_route(self):
-
         with app.test_client() as tc:
-
             response = tc.get('/cases')
             self.assertEqual(response.status_code, 200)
 
     # Test recoveries route
     def test_recoveries_route(self):
-
         with app.test_client() as tc:
-
             response = tc.get('/recoveries')
             self.assertEqual(response.status_code, 200)
 
     # Test deaths route
     def test_deaths_route(self):
-
         with app.test_client() as tc:
-
             response = tc.get('/deaths')
             self.assertEqual(response.status_code, 200)
-    
+
     # Test country specific cases route, with Benin as an example
     def test_country_cases(self):
-
         with app.test_client() as tc:
-
             response = tc.get('/Benin')
             self.assertEqual(response.status_code, 200)
