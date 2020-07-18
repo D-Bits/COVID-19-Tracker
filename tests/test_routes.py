5008caedb3725147305a180572ec98dad5d113b6
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-07-18 11:34:35 -0700

    Updated unit tests.

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -10,44 +10,57 @@ class RoutesTests(TestCase):
 
     # Test index route(s) returns a 200 response
     def test_index_route(self):
+
         with app.test_client() as tc:
+
             response = tc.get('/')
-            redirect_response = tc.get('/home')
             self.assertEqual(response.status_code, 200)
-            self.assertEqual(redirect_response.status_code, 200)
+
 
     # Test about page route
     def test_about_route(self):
+
         with app.test_client() as tc:
+
             response = tc.get('/about')
             self.assertEqual(response.status_code, 200)
 
     # Test cases route
     def test_cases_route(self):
+
         with app.test_client() as tc:
+
             response = tc.get('/cases')
             self.assertEqual(response.status_code, 200)
 
     # Test recoveries route
     def test_recoveries_route(self):
+
         with app.test_client() as tc:
+
             response = tc.get('/recoveries')
             self.assertEqual(response.status_code, 200)
 
     # Test deaths route
     def test_deaths_route(self):
+
         with app.test_client() as tc:
+
             response = tc.get('/deaths')
             self.assertEqual(response.status_code, 200)
 
     # Test country specific cases route, with Benin as an example
     def test_country_cases(self):
+
         with app.test_client() as tc:
+
             response = tc.get('/country/Denmark')
             self.assertEqual(response.status_code, 200)
 
     # Test country totals history route
     def test_country_cases(self):
+
         with app.test_client() as tc:
+            
             response = tc.get('/totals/Brazil')
             self.assertEqual(response.status_code, 200)
