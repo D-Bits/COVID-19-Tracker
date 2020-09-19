be2e7cb84e8a437a5d811468e0f18205c44defae
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-09-19 12:59:37 -0700

    Added unit tests for u.s. data.

---
+++ tests/test_usa.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+from app import app
+
+"""
+Unit testing for routes in U.S. data blueprint
+"""
+
+class USRoutesTests(TestCase):
+
+    def test_us_summary(self):
+
+        with app.test_client() as tc:
+
+            response = tc.get('/us/summary')
+
+    def test_us_cases(self):
+
+        with app.test_client() as tc:
+
+            response = tc.get('/us/cases')
+
+    def test_us_deaths(self):
+
+        with app.test_client() as tc:
+
+            response = tc.get('/us/deaths')
