7dbb9b6c20c6613db1d6df9e9d38d66dd38c4d88
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-09-21 11:55:25 -0700

    Updated USA blueprint unit tests.

--- tests/test_usa.py
+++ tests/test_usa.py
@@ -12,21 +12,25 @@ class USRoutesTests(TestCase):
         with app.test_client() as tc:
 
             response = tc.get('/us/summary')
+            self.assertEqual(response.status_code, 200)
 
     def test_us_cases(self):
 
         with app.test_client() as tc:
 
             response = tc.get('/us/cases')
+            self.assertEqual(response.status_code, 200)
 
     def test_us_deaths(self):
 
         with app.test_client() as tc:
 
             response = tc.get('/us/deaths')
+            self.assertEqual(response.status_code, 200)
 
     def test_us_visualizations(self):
 
         with app.test_client() as tc:
 
             response = tc.get('/us/graphs/wa')
+            self.assertEqual(response.status_code, 200)
