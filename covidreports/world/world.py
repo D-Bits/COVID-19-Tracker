1032ac81d977f018507f23bf6e219225885a2700
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-10-16 01:30:41 -0700

    Cleaned up old template code.

--- covidreports/world/world.py
+++ covidreports/world/world.py
@@ -47,7 +47,7 @@ def index():
 @world_bp.route('/about')
 def about():
 
-    return render_template('about.html')
+    return render_template('about.html', title="About")
 
 
 # Route for cases page
