588ddf44f45e05e26012bad22575aa03cf325242
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-11-17 01:17:36 -0800

    Consolidated a whole bunch of code.

--- covidreports/static/js/main.js
+++ covidreports/static/js/main.js
@@ -33,57 +33,43 @@ function findData(tableId, inputId, index) {
 
 // Function for sorting column values
 // NOT YET WORKING
-function sortTable(n, tableId) {
-  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-  table = document.getElementById(tableId);
-  switching = true;
-  // Set the sorting direction to ascending:
-  dir = "asc";
-  /* Make a loop that will continue until
-  no switching has been done: */
-  while (switching) {
-    // Start by saying: no switching is done:
-    switching = false;
-    rows = table.rows;
-    /* Loop through all table rows (except the
-    first, which contains table headers): */
-    for (i = 1; i < (rows.length - 1); i++) {
-      // Start by saying there should be no switching:
-      shouldSwitch = false;
-      /* Get the two elements you want to compare,
-      one from current row and one from the next: */
-      x = rows[i].getElementsByTagName("TD")[n];
-      y = rows[i + 1].getElementsByTagName("TD")[n];
-      /* Check if the two rows should switch place,
-      based on the direction, asc or desc: */
-      if (dir == "asc") {
-        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-          // If so, mark as a switch and break the loop:
-          shouldSwitch = true;
-          break;
-        }
-      } else if (dir == "desc") {
-        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-          // If so, mark as a switch and break the loop:
-          shouldSwitch = true;
-          break;
-        }
-      }
+// Basic example
+var asc = 0;
+function sort_table(table, col) {
+  $('.sortorder').remove();
+  if (asc == 2) { asc = -1; } else { asc = 2; }
+  var rows = table.tBodies[0].rows;
+  var rlen = rows.length - 1;
+  var arr = new Array();
+  var i, j, cells, clen;
+  // fill the array with values from the table
+  for (i = 0; i < rlen; i++) {
+    cells = rows[i].cells;
+    clen = cells.length;
+    arr[i] = new Array();
+    for (j = 0; j < clen; j++) { arr[i][j] = cells[j].innerHTML; }
+  }
+  // sort the array by the specified column number (col) and order (asc)
+  arr.sort(function (a, b) {
+    var retval = 0;
+    var col1 = a[col].toLowerCase().replace(',', '').replace('$', '').replace(' usd', '')
+    var col2 = b[col].toLowerCase().replace(',', '').replace('$', '').replace(' usd', '')
+    var fA = parseFloat(col1);
+    var fB = parseFloat(col2);
+    if (col1 != col2) {
+      if ((fA == col1) && (fB == col2)) { retval = (fA > fB) ? asc : -1 * asc; } //numerical
+      else { retval = (col1 > col2) ? asc : -1 * asc; }
     }
-    if (shouldSwitch) {
-      /* If a switch has been marked, make the switch
-      and mark that a switch has been done: */
-      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      switching = true;
-      // Each time a switch is done, increase this count by 1:
-      switchcount ++;
-    } else {
-      /* If no switching has been done AND the direction is "asc",
-      set the direction to "desc" and run the while loop again. */
-      if (switchcount == 0 && dir == "asc") {
-        dir = "desc";
-        switching = true;
-      }
-    }
+    return retval;
+  });
+  for (var rowidx = 0; rowidx < rlen; rowidx++) {
+    for (var colidx = 0; colidx < arr[rowidx].length; colidx++) { table.tBodies[0].rows[rowidx].cells[colidx].innerHTML = arr[rowidx][colidx]; }
+  }
+
+  hdr = table.rows[0].cells[col];
+  if (asc == -1) {
+    $(hdr).html($(hdr).html() + '<span class="sortorder">▲</span>');
+  } else {
+    $(hdr).html($(hdr).html() + '<span class="sortorder">▼</span>');
   }
 }
